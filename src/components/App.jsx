530f15118da52698e0c8656965b77e82d55cbe5e
Lemuel Benitez <lemuelbenitez@gmail.com>
2024-06-04 11:06:46 -0400

    added functional programming to initiate card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,22 @@ import Card from "./Card";
 import contacts from "../contacts";
 
 function App() {
+  function createCard(contacts){
+
+     return( <Card 
+        key ={contacts.id}
+        name= {contacts.name}
+        imgURL ={contacts.imgURL}
+        phone = {contacts.phone}
+        email = {contacts.email}
+      />
+     )
+  }
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
+     {contacts.map(createCard)}
+      {/* <Card
         {...contacts[0]}
       />
       <Card
@@ -14,7 +26,7 @@ function App() {
       />
       <Card
         {...contacts[2]}
-      />
+      /> */}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-function Card({name, imgURL, phone, email}) {
+function Card(props) {
   return (
     <div className="card">
       <div className="top">
-        <h2 className="name">{name}</h2>
-        <img className="circle-img" src={imgURL} alt="avatar_img" />
+        <h2 className="name">{props.name}</h2>
+        <img className="circle-img" src={props.imgURL} alt="avatar_img" />
       </div>
       <div className="bottom">
-        <p className="info">{phone}</p>
-        <p className="info">{email}</p>
+        <p className="info">{props.phone}</p>
+        <p className="info">{props.email}</p>
       </div>
     </div>
   );
